7c0e86daf5c7e31753c62f0860988e10897ac964
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-30 15:08:22 -0300

    Listado os times por meio do método GET

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+  ];
+
 const app = express();
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'Olá, mundo!' }));
+
+app.get('/teams', (_req, res) => res.status(200).json({ teams }));
 
 module.exports = app;
