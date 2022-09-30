54f3e511562649cff234d114909b0314b741df77
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-30 15:22:07 -0300

    Cadastrado times por meio do método POST

--- src/app.js
+++ src/app.js
@@ -15,8 +15,16 @@ const teams = [
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (_req, res) => res.status(200).json({ message: 'Olá, mundo!' }));
 
 app.get('/teams', (_req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+    res.status(201).json({ teams: newTeam });
+});
 
 module.exports = app;
