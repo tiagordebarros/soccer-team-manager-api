44c59161557077876562962c3558b2b34073b3c2
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-30 23:57:00 -0300

    Apagado os times por meio do método DELETE

--- src/app.js
+++ src/app.js
@@ -54,4 +54,12 @@ app.get('/teams/:id', (req, res) => {
     return res.status(200).json(filterId);
 });
 
+app.delete('/teams/:id', (req, res) => {
+    const { id } = req.params;
+
+    const deleteId = teams.filter((team) => team.id !== Number(id));
+
+    return res.status(200).json(deleteId);
+});
+
 module.exports = app;
