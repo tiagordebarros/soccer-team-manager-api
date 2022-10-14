d26ad451ffc5c051ce64872808885bb67a6072dd
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-14 18:30:44 -0300

    Implementado middleware para apresentar no terminal os logs das reqs em tempo real

--- src/app.js
+++ src/app.js
@@ -22,6 +22,16 @@ app.use(express.static('./images'));
 app.use(express.json());
 app.use(apiCredentials);
 
+app.use((req, _res, next) => {
+  console.log('req.method:', req.method);
+  console.log('req.path:', req.path);
+  console.log('req.params:', req.params);
+  console.log('req.query:', req.query);
+  console.log('req.headers:', req.headers);
+  console.log('req.body:', req.body);
+  next();
+});
+
 const existingId = (req, res, next) => {
   const { id } = req.params;
   if (teams.some((team) => team.id === Number(id))) return next();
